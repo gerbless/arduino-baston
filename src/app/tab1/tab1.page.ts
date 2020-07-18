0204f74aa56ff4ff88bcf4307693a240f78f72ea
Flavio <gerbless@gmail.com>
2020-07-17 20:27:43 -0400

    inicial

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx'
+import {AlertController} from '@ionic/angular'
+import { Sauce } from 'protractor/built/driverProviders';
+import { ConsoleReporter } from 'jasmine';
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+Devices
+  constructor(private bluetoothSerial:BluetoothSerial,private alertController: AlertController) {}
+
+  ActivarBluetooth(){
+  this.bluetoothSerial.isEnabled().then(response=>{
+    //this.isEnabled("IsOn");
+    this.Listdivices()
+  },error=>{
+    this.isEnabled("IsOff");
+  })
+}
+listdivices (){
+  this.bluetoothSerial.list().then(response=>{ 
+   this.Devices=response
+  },error=> {
+    console.log("error")
+  }
+  )
+}
+
+connect(address){
+  this.bluetoothSerial.connect(address).subscribe(succes=>{
+   this.deviceConnected()  
+  },error=>{
+  console.log("error")
+  })
+}
+
+deviceConnected(){
+  this.bluetoothSerial.subscribe('/n').subscribe(success=>(
+   this.hundler(success)
+  })
+}
+
+hundler(){
+  console.log(value)
+}
+
+sebData(){
+  this.bluetoothSerial.write("7").then(reponse=>{
+    console.log("ok")
+  },error=>{
+  console.log("a problem")
+  }
+  )
+}
+
+Disconected(){
+  this.bluetoothSerial.disconect()
+  console.log("dispositivo desconectado")
+}
+
+async isEnabled(msg){
+const alert =await this.alertController.create({
+  header: 'Alerta',
+  message:msg,
+  buttons:[{
+    text:'ok'
+    handler:()=>{
+      console.log("ok")
+    }
+   }]
+  })
+ }
+}
